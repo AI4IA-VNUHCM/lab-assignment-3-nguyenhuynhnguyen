ad03085990e0ca8f08b2edc939d2b6d1e79fbf6b
nguyenhuynhnguyen <91869289+nguyenhuynhnguyen@users.noreply.github.com>
2021-10-18 14:44:12 +0700

    Update Exercise1.c

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -13,8 +13,14 @@ Ex:
 #include <math.h>
 
 void Ex1(int arr[], int n){
-	//Your codes here
-    
+	for (int i = 0, j = n - 1; i < j; i++, j--)
+	{
+		if (arr[i] != arr[j]) {
+			printf("asymmetric");
+			break;
+		}
+	}
+	printf("symmetric");
 }
 
 int main(int argc, char *argv[]) {
